7a15cc12f2d0df5364df9ef6ae476471b2c387fe
Jajared <jajawong09@gmail.com>
2026-03-30 13:31:25 +0800

    Attempt to fix caching again

--- framework/utils/cache.py
+++ framework/utils/cache.py
@@ -1,12 +1,16 @@
 from fastapi import Response
 
-def cdn_cache(max_age: int):
+
+def cdn_cache(max_age: int = 1):
     """
     FastAPI dependency to set a 'Cache-Control' header for CloudFront.
+    Defaults to 1 second if no max_age is provided.
     """
+
     async def set_cache_control_header(response: Response):
-        if max_age == 0:
-            response.headers["Cache-Control"] = "public, no-cache, no-store, must-revalidate, max-age=0"
-        else:
-            response.headers["Cache-Control"] = f"public, max-age={max_age}"
+        # We enforce a minimum of 1 to align with CloudFront's CachingOptimized Minimum TTL
+        effective_age = max(max_age, 1)
+
+        response.headers["Cache-Control"] = f"public, max-age={effective_age}"
+
     return set_cache_control_header
